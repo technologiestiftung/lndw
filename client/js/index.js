60cba77e72a311885d04bb640db331c6bc63b684
fdnklg <fabian.dinklage@me.com>
2018-05-16 18:36:13 +0200

    webcam view added

--- client/js/index.js
+++ client/js/index.js
@@ -1,31 +1,22 @@
-var video;
-var vidWidth = 320;
-var vidHeight = 240;
+var vidWidth = 1000;
+var vidHeight = 800;
+var title, info, prompt, video, container, devices, id;
 
 function init() {
- 	//init webcam texture
-     video = document.createElement('video');
-     video.width = vidWidth;
-     video.height = vidHeight;
-     video.autoplay = true;
-     video.loop = true;
- 
-     //make it cross browser
-     window.URL = window.URL || window.webkitURL;
-     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-     //get webcam
-     navigator.getUserMedia({
-         video: true
-     }, function(stream) {
-         //on webcam enabled
-         video.src = window.URL.createObjectURL(stream);
-         prompt.style.display = 'none';
-         title.style.display = 'inline';
-         container.style.display = 'inline';
-         gui.domElement.style.display = 'inline';
-     }, function(error) {
-         prompt.innerHTML = 'Unable to capture WebCam. Please reload the page.';
-     });   
+    video = document.querySelector('#video');
+    video.width = vidWidth;
+    video.height = vidHeight;
+    video.autoplay = true;
+
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+        var camera = devices.filter(device => device.kind == "videoinput");
+        camera.forEach(device => {
+            if(device.label == "USB 2.0 Camera (0c45:6340)") { id = device.deviceId;};
+        })
+        var constraints = { deviceId: { exact: id } };
+        return navigator.mediaDevices.getUserMedia({ video: constraints });
+    })
+    .then(stream => video.srcObject = stream);
 }
 
 init();
