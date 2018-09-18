0c178967b747a42878572dd6077d3a0c45de7c19
Sebastian Meier <contact@sebastianmeier.eu>
2018-09-18 11:32:27 +0200

    html server

---
+++ snap-server/html_server.js
@@ -0,0 +1,19 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+
+const requestHandler = (request, repsonse) => {
+	if(request.url.indexOf('images')>=0){
+		response.end(fs.readFileSync('.' + request.url))
+	}else{
+		response.end(fs.readFileSync('./html/generated.html', 'utf8'))
+	}
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, err => {
+	if(err) return console.log('something bad happened', err)
+
+	console.log(`Server is listening on ${port}`)
+})
